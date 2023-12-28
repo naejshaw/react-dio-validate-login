4d6ccaf6e222456af596fd679f3b10b8afe6864d
JeanFDev <jeanjfra1@gmail.com>
2023-12-27 23:27:33 -0300

    update disable

--- src/components/Button/types.ts
+++ src/components/Button/types.ts
@@ -1,4 +1,5 @@
-export interface IButtonProps {
+export interface IButtonProps extends React.ButtonHTMLAttributes<HTMLButtonElement>  {
     title: string;
     onClick?: React.MouseEventHandler<HTMLButtonElement>;
+    disabled: boolean;
 }
